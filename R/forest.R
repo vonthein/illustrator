2927597168c3c145b3deb5f11a6c1dbe5b37b862
Reinhard Vonthein <vonthein@imbs.uni-luebeck.de>
2017-07-13 09:43:40 +0200

    adds parameters

--- R/forest.R
+++ R/forest.R
@@ -20,6 +20,9 @@
 #' @param sizeratio factor by which symbols in right stand are larger
 #' @param colri extreme colors of right stand
 #' @param colle extreme colors of left stand
+#' @param iex symbol expansion factor
+#' @param ip symbol height factor
+#' @param seed for random number generator
 #' @param ... input to function lines()
 #'
 #' @return plot of two groups of symbols in rows and columns or uniformly distributed
@@ -43,11 +46,17 @@ Forest <-function(ni = 30,        # trees per stand
  icon = fir, sizeratio = 1.4,     # right icons larger
  colri = c("green","lightgreen"), # colorpalette right
  colle = c("darkgreen","green"),  # colorpalette left
- ...){
+ iex  = 1, ip = 1, # icon size and proportion
+ seed  = 12345,
+ ...) {
+ set.seed(seed)
+ icon <- iex * cbind(icon[,1], ip * icon[,2])
+ par(c(0, 0, 0, 0) + 0.1)
  if(nrow>0) {
   nij<- (ni %/% nrow) + 1 # firs per row
   sb <- path + 14*nrow # width of stands
-  plot(c(-sb,sb),c(-near,(10 - near+1)*nij+2),pch="")
+  plot(c(-sb,sb),c(-near,(10 - near+1)*nij+2),pch="", xlab="", ylab="",
+       xaxt  = "n", yaxt  = "n", bty = "n")
   # coordinates of twig tips
   x1 <- path + rep(14*(0:(nrow - 1)),nij)[1:ni] + runif(ni,-1,1)
   y1 <- 0+rep((10 - near)*((nij - 1):0),each = nrow, len = ni) + runif(ni,-1,1)
@@ -55,7 +64,8 @@ Forest <-function(ni = 30,        # trees per stand
   x2 <- path + rep(14*(0:(nrow - 1)),nij)[1:ni] + runif(ni,-1,1)
   y2 <- 0+rep((10 - near)*((nij - 1):0),each = nrow, len = ni) + runif(ni,-1,1)
  } else {
-  plot(c(-80,80),c(-30,45),pch="")
+  plot(c(-80,80),c(-30,45),pch="", xlab="", ylab="",
+       xaxt  = "n", yaxt  = "n", bty = "n")
   x1 <- runif(ni,path,60)   # coordinates of twig tips
   y1 <- runif(ni,-30,30)
   x1 <- x1[order(y1,decreasing=T)]
@@ -69,11 +79,12 @@ Forest <-function(ni = 30,        # trees per stand
                          as.vector(outer(-1 *icon[,1],-x2,"+"))),
                  c(as.vector(outer(sizeratio*icon[,2], y1,"+")),
                          as.vector(outer(    icon[,2], y2,"+"))))
+ lines(stands,...)
  polygon(stands[(1:length(stands)/4),],
          col=shape::shadepalette(ni,colri[1],colri[2]))
  polygon(stands[-(1:(length(stands)/4)),],
          col=shape::shadepalette(ni,colle[1],colle[2]))
- lines(stands,...)
+ par(c(5, 4, 4, 2) + 0.1)
 }
 
 
